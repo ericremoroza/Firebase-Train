a3b1ab756480bf2042f943c964a5fd9f8f7aec26
ericremoroza <eric.remoroza@outlook.com>
2018-09-27 09:57:35 -0400

    added background image

---
+++ README.md
@@ -0,0 +1,3 @@
+Firebase-Train
+
+Background image taken from YouTube thumbnail (https://www.youtube.com/watch?v=beLZq2webeQ).
---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAK-QSTmJVas9LMXxlPquDBLxIFUXzxmuw",
+    authDomain: "fir-train-bb36a.firebaseapp.com",
+    databaseURL: "https://fir-train-bb36a.firebaseio.com",
+    projectId: "fir-train-bb36a",
+    storageBucket: "",
+    messagingSenderId: "97902743591"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#newTrain").on("click", function() {
+    
+  })
